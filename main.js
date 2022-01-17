781c6b0639785820203520b59561a2ebe950ba8f
jeongseonmi <misoda1004@gmail.com>
2022-01-17 16:45:26 +0900

    class함수복습

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
-const boxEl = document.querySelector('.box');
 
-//삼항연산자
-console.log(a ? 'true' : 'false')
+
+// function User(first, last){
+//     this.firstName = first
+//     this.lastName = last
+// }
+
+// User.prototype.getFullName = function(){
+//     return `${this.firstName} ${this.lastName}`
+// }
+
+class User{
+    constructor(first, last){
+        this.firstName = first
+        this.lastName = last
+    }
+    getFullName(){
+        return `${this.firstName} ${this.lastName}`   
+    }
+}
+
+const heropy = new User('heropy', 'park')
+const amy = new User('amy', 'joo')
+
+console.log(heropy.getFullName())
+console.log(amy)
